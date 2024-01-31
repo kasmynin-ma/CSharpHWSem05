342f962a966bbe37c1d43c84563f48a4323c4a48
Maxim Kasmynin <kasmynin.m@gmail.com>
2024-01-31 15:00:03 +0300

    Task01 completed

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = InputNum("Введите число n: ");
+int num2 = InputNum("Введите число m: ");
+
+int funAkk = FunctionAkkerman(num1, num2);
+Console.Write($"Функция Аккермана = {funAkk} ");
+Console.ReadLine();
+int FunctionAkkerman(int num1, int num2)
+{
+    if (num1 == 0) 
+    {
+        return num2 + 1;
+    }
+
+    if (num2 == 0) 
+    {
+        return FunctionAkkerman(num1 - 1, 1);
+    }
+    else 
+    {
+        return FunctionAkkerman(num1 - 1, FunctionAkkerman(num1, num2 - 1));
+    }
+}
+
+int InputNum(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
