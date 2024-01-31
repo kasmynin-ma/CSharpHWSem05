30d4d04366182181c4766cbc849f5f8917b7a36b
Maxim Kasmynin <kasmynin.m@gmail.com>
2024-01-31 15:29:02 +0300

    Task02 completed

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = GetArrayRndInt();
+PrintArray(array);
+Console.WriteLine();
+OutputReverseArray(array);
+Console.ReadLine();
+
+void OutputReverseArray(int[] a, int i = 0)
+{
+    if (i > a.Length - 1)
+        return;
+    OutputReverseArray(a, i + 1);
+    Console.Write(" " + a[i]);
+}
+int[] GetArrayRndInt()
+{
+    Random rnd = new Random();
+    int[] array = new int[rnd.Next(15)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(15);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Полученный массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
